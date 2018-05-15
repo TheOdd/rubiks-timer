f15a38a9759f75215119b4f88e313198a2684af5
TheOdd <owenhines07@gmail.com>
2018-05-15 10:52:09 -0500

    Added react-native-vector-icons for nav icon. Configured Stopwatch tab.

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { configureStore, history } from './configureStore'
 import Root from './containers/Root'
+import { createBottomTabNavigator } from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const store = configureStore()
 
-export default class App extends React.Component {
+class Stopwatch extends React.Component {
     render() {
         return (
             <Root store={store} history={history}/>
         )
     }
 }
+
+export default createBottomTabNavigator({
+    Stopwatch: {
+        screen: Stopwatch,
+        navigationOptions: {
+            tabBarLabel: 'Stopwatch',
+            tabBarIcon: ({ tintColor, focused }) => (
+                <Ionicons
+                    name={focused ? 'ios-stopwatch' : 'ios-stopwatch-outline'}
+                    size={26}
+                    style={{ color: tintColor }}
+                />
+            )
+        }
+    }
+}, {
+    initialRouteName: 'Stopwatch'
+})
